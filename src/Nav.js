a5e560619869fbfb9a88f59a43b2e4e5650e0954
juliasfrohlich <juliasfrohlich@hotmail.com>
2021-05-14 09:22:12 -0300

    feat(Netflix Experience): Add frontend structure

---
+++ src/Nav.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Nav.css';
+
+function Nav() {
+  return (
+      <div className='nav'>
+        <img src ="https://logos-world.net/wp-content/uploads/2020/04/Netflix-Logo.png" alt="" />
+       <h1>This is  the nav</h1>
+      </div>
+  )
+}
+
+export default Nav
---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAzMXlzS-RUihGl8OWW2DB5w-PUSWE-f8A",
+  authDomain: "netflix-experience.firebaseapp.com",
+  projectId: "netflix-experience",
+  storageBucket: "netflix-experience.appspot.com",
+  messagingSenderId: "979717889419",
+  appId: "1:979717889419:web:07856ceded4c0e15c1a38e"
+};
